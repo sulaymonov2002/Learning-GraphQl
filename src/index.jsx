ff79abb7ae87ea0c6c1387db5a3fa4f3af8d443e
sulaymonov2002 <ssamir.web2002@gmail.com>
2022-11-24 11:55:16 +0500

    add new input and buutton done all functionalty

---
+++ src/index.jsx
@@ -0,0 +1,144 @@
+import React, { useState } from "react";
+import * as ReactDOM from "react-dom/client";
+import App from "./App";
+
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+  useLazyQuery,
+  useMutation,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://sxewr.sse.codesandbox.io/",
+  cache: new InMemoryCache(),
+});
+
+const GET_TODOS = gql`
+  {
+    todos {
+      id
+      type
+    }
+  }
+`;
+
+const UPDATE_TODO = gql`
+  mutation UpdateTodo($id: String!, $type: String!) {
+    updateTodo(id: $id, type: $type) {
+      id
+      type
+    }
+  }
+`;
+
+function Todos() {
+  const { loading, error, data } = useQuery(GET_TODOS);
+  const [updateTodo, { loading: mutationLoading, error: mutationError }] =
+    useMutation(UPDATE_TODO);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.todos.map(({ id, type }) => {
+    let input;
+
+    return (
+      <div key={id}>
+        <p>{type}</p>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            updateTodo({ variables: { id, type: input.value } });
+
+            input.value = "";
+          }}
+        >
+          <input
+            ref={(node) => {
+              input = node;
+            }}
+          />
+          <button type="submit">Update Todo</button>
+        </form>
+        {mutationLoading && <p>Loading...</p>}
+        {mutationError && <p>Error :( Please try again </p>}
+      </div>
+    );
+  });
+}
+
+const ADD_TODO = gql`
+  mutation AddTodo($type: String!) {
+    addTodo(type: $type) {
+      id
+      type
+    }
+  }
+`;
+
+function AddTodo() {
+  let input;
+  const [addTodo] = useMutation(ADD_TODO, {
+    update(cache, { data: { addTodo } }) {
+      cache.modify({
+        fields: {
+          todos(existingTodos = []) {
+            const newTodoRef = cache.writeFragment({
+              data: addTodo,
+              fragment: gql`
+                fragment NewTodo on Todo {
+                  id
+                  type
+                }
+              `,
+            });
+            return existingTodos.concat(newTodoRef);
+          },
+        },
+      });
+    },
+  });
+
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          addTodo({ variables: { type: input.value } });
+          input.value = "";
+        }}
+      >
+        <input
+          ref={(node) => {
+            input = node;
+          }}
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+    </div>
+  );
+}
+
+function Text() {
+  return (
+    <ApolloProvider client={client}>
+      <div>
+        <h2>Building Query components 🚀</h2>
+        <AddTodo />
+        <Todos />
+      </div>
+    </ApolloProvider>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <>
+    <App />
+    <Text />
+  </>
+);
